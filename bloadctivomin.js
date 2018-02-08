6adb6c0fb51fc94584b4de0c538460e5f65645b7
Ackter <36087074+Ackter@users.noreply.github.com>
2018-02-07 23:00:14 -0700

    Add files via upload

---
+++ bloadctivomin.js
@@ -0,0 +1,22 @@
+function ventanaSecundaria(){
+ t=12;
+ timer=window.setInterval("contador()",1000);
+   pop=window.open("http://ouo.io/7ckLUM","ventana1","width=480,height=325,scrollbars=0")
+}
+function contador(){
+  if(pop!=null){
+    if(pop.closed){
+      swal("No Cierres La Publicidad!", "Este sitio se mantiene gracias a eso!", "error");
+      clearInterval(timer);
+      t='.time.';
+      document.getElementById("contador").value="Mostrar enlaces";
+      return
+    }
+    if(pop.frames.length!=3){ 
+   t=t-1;
+   if(t<=0){
+     clearInterval(timer);
+ document.getElementById("hide").style.display="";
+ t="Enlaces"}document.getElementById("contador").value=t;
+ return false
+ }}}
